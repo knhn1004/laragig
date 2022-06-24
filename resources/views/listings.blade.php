565f653aece059933de69b194c374d9653694197
oliver <kunhan945@gmail.com>
2022-06-24 16:10:57 +0800

    listings.blade.php

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,17 +1,26 @@
-<h1>{{ $heading }}</h1>
+@extends('layout')
+@section('content')
+<h1>Laragigs</h1>
 
-
+<div>
 @if(count($listings) > 0)
 	@foreach($listings as $listing)
 	<h2>
 		<a href="/listings/{{$listing['id']}}">
-		{{ $listing['title'] }}
+		{{ $listing->title }}
 		</a>
 	</h2>
-	<p>
-		{{ $listing['description'] }}
-	</p>
+	<h3>{{ $listing->company }}</h3>
+	<ul>
+		<li>Laravel</li>
+		<li>Vue.js</li>
+		<li>Next.js</li>
+	</ul>
+	<div>{{ $listing->location }}</div>
 	@endforeach
 @else
 	<p>No Listings Available</p>
 @endif
+</div>
+
+@endsection
