dd66a92aa3da0590b07b3cb33a37b8c8b19cc611
oliver <kunhan945@gmail.com>
2022-06-24 15:45:42 +0800

    seeding

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing
-{
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis eius mollitia culpa incidunt nam ab laudantium ipsam laborum provident quas?'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis eius mollitia culpa incidunt nam ab laudantium ipsam laborum provident quas?'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id)
-    {
-        $listings = self::all();
-        foreach ($listings as $l) {
-            if ($l['id'] == $id) {
-                return $l;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
